4f1ead5f221f2e6b198b35b3f7520c3fb4fa8feb
henizola <z.w.henok@gmail.com>
2020-04-10 19:29:55 +0300

    Updates

---
+++ precache-manifest.71529b267d6b1a3ab426051ad27b12eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815dd63f2d184094b04141a5a2b99f93",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3b4a51680bb3dd051629",
+    "url": "/monsters/static/css/main.64cbffea.chunk.css"
+  },
+  {
+    "revision": "3056e8cfc071591d8a98",
+    "url": "/monsters/static/js/2.d4d411b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.d4d411b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4a51680bb3dd051629",
+    "url": "/monsters/static/js/main.c7b62dc6.chunk.js"
+  },
+  {
+    "revision": "8c2a78da04dec2172d77",
+    "url": "/monsters/static/js/runtime-main.9cfc745b.js"
+  }
+]);
